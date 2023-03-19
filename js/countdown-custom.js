499e51cb655441bfc9788b8ec78fa160f5b35c41
3m1ss4ry <109291690+3m1ss4ry@users.noreply.github.com>
2023-03-19 12:36:51 +0100

    Add files via upload

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2023, 8, 2, 6)}); // year, month, date, hour
+        });
+});		
+
